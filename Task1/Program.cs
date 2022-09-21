147efee40d72e65b8e45fb34ac6326883d56f1f8
Elena <senusya@gmail.com>
2022-09-21 18:24:56 +0300

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Write first number: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write second number: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"max = " + number1);
+}
+else
+{
+    Console.WriteLine($"max = " + number2);
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Write first number: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write second number: ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write third number: ");
+int number3 = int.Parse(Console.ReadLine());
+
+if ((number1 > number2) &  (number1 > number3))
+{
+    Console.WriteLine($"max = " + number1);
+}
+else if ((number2 > number1) & (number2 > number3))
+{
+    Console.WriteLine($"max = " + number2);
+}
+else
+{
+    Console.WriteLine($"max = " + number3);
+}
