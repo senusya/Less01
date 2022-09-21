fb6717227b32b1c85c79c3de3c3bf2dd2f583f07
Elena <senusya@gmail.com>
2022-09-21 19:03:28 +0300

    Add task 3

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Write a number: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+int count = 1;
+
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count ++;
+}
