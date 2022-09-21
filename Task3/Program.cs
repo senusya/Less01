36d7d1d8775a7c095869df1396ba733d53c9a843
Elena <senusya@gmail.com>
2022-09-21 18:44:33 +0300

    Add task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write a number: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+if (number % 2 == 0 ) 
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
