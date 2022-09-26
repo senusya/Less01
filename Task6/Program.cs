51b8b97d1e936f416d0b8dcc074a5008cdf39976
Elena <senusya@gmail.com>
2022-09-26 18:08:53 +0300

    Add HW2 Task 2

---
+++ Task6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Write number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("There is no third digit");
+}
+else 
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine(number % 10);
+}
