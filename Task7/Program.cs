ffb12a2982100dd6a15fe4acec48a7407b8de5bc
Elena <senusya@gmail.com>
2022-09-26 18:44:50 +0300

    Add HW2 Task3

---
+++ Task7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write number: ");
+int number = int.Parse(Console.ReadLine());
+
+if ((number == 6) || (number == 7))
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
