aaa5d7ab60e226d63e025ff57c77145e0d84685b
Elena <senusya@gmail.com>
2022-09-29 18:42:21 +0300

    Add HW3 task 1

---
+++ HW3_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write a five-digit number: ");
+string fiveDigitString = Console.ReadLine()!;
+
+for (int i = 0; i < fiveDigitString.Length / 2; i++)
+{
+    if (fiveDigitString[i] != fiveDigitString[fiveDigitString.Length - 1 - i])
+    {
+        Console.WriteLine("no");
+        return;
+    }
+}
+Console.WriteLine("yes");
