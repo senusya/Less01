0e8db56ef9c320b8f3186ac01eb4d6ca517d8b10
Elena <senusya@gmail.com>
2022-09-29 18:54:45 +0300

    Add HW3 task 2

---
+++ HW3_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Write coordinates for XA: ");
+double xa = double.Parse(Console.ReadLine()!);
+Console.Write("Write coordinates for YA: ");
+double ya = double.Parse(Console.ReadLine()!);
+Console.Write("Write coordinates for ZA: ");
+double za = double.Parse(Console.ReadLine()!);
+
+Console.Write("Write coordinates for XB: ");
+double xb = double.Parse(Console.ReadLine()!);
+Console.Write("Write coordinates for YB: ");
+double yb = double.Parse(Console.ReadLine()!);
+Console.Write("Write coordinates for ZB: ");
+double zb = double.Parse(Console.ReadLine()!);
+
+double result = Math.Sqrt(Math.Pow(xb - xa,2) + Math.Pow(yb - ya,2) + Math.Pow(zb - za,2));
+Console.WriteLine(result);
