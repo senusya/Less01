aecd27576969c54a8d51d5799b6fc9e453f4097c
Elena <senusya@gmail.com>
2022-10-03 18:50:20 +0300

    Add HW4 task 2

---
+++ HW4_2/Program.cs
@@ -0,0 +1,17 @@
+﻿int SumOfDigits(string digits)
+{
+    int result = 0;
+
+    for (int i = 0; i < digits.Length; i++)
+    {
+        result = result + int.Parse(digits[i].ToString());
+    }
+
+    return result;
+}
+
+
+Console.Write("Write a number: ");
+string number = Console.ReadLine()!;
+int result = SumOfDigits(number);
+Console.WriteLine(result);
