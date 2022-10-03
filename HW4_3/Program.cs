0a14390613299df1a5e99512bf478db266839ddd
Elena <senusya@gmail.com>
2022-10-03 18:57:14 +0300

    Add HW4 task 3

---
+++ HW4_3/Program.cs
@@ -0,0 +1,32 @@
+﻿void FillArray(int[] array)
+{
+    Random randGenerator = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = randGenerator.Next(1, 100);
+    }
+}
+ 
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+ 
+
+int arrayLenght = 8;
+ 
+int[] array = new int[arrayLenght];
+ 
+FillArray(array);
+PrintArray(array);
+ 
+ 
