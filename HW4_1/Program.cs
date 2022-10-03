9399439acaf61371c577faf48471c8b2358ad9a9
Elena <senusya@gmail.com>
2022-10-03 18:25:52 +0300

    Add HW4 task 1

---
+++ HW4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿int Power(int firstNumber, int secondNumber)
+{
+    int result = firstNumber;
+
+    while (secondNumber > 1)
+    {
+        result = result * firstNumber;
+        secondNumber--;
+    }
+
+    return result;
+}
+
+Console.WriteLine("Write a first number: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Write a second number: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+int result = Power(firstNumber, secondNumber);
+
+Console.WriteLine(result);
