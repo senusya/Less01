833be8b700d005c47042585e746281ee7b749787
Elena <senusya@gmail.com>
2022-10-05 22:06:36 +0300

    Add HW5 task 2

---
+++ HW5_2/Program.cs
@@ -0,0 +1,44 @@
+﻿int[] GetArray(int arrayLenght, int min, int max)
+{
+    int[] array = new int[arrayLenght];
+    Random randGenerator = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = randGenerator.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]\n");
+}
+
+int GetOddSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+
+
+int[] array = GetArray(8, 10, 99);
+PrintArray(array);
+Console.WriteLine(GetOddSum(array));
+
