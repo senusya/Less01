e3cd25871e24b254faed8bac344e85358d0160af
Elena <senusya@gmail.com>
2022-10-09 20:31:31 +0300

    Add HW 6 task 1

---
+++ HW6_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Write a count of numbers: ");
+int countOfNumbers = int.Parse(Console.ReadLine()!);
+
+int countOfPositiveNumbers = 0;
+
+while (countOfNumbers > 0)
+{
+    Console.Write("Write a number: ");
+    int number = int.Parse(Console.ReadLine()!);
+    if (number > 0)
+    {
+        countOfPositiveNumbers++;
+    }
+    countOfNumbers--;
+}
+
+Console.Write(countOfPositiveNumbers);
