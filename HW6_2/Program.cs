39227f79a2751731f10e28c2358e9573a26cc768
Elena <senusya@gmail.com>
2022-10-09 20:44:20 +0300

    Add HW6 task 2

---
+++ HW6_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Write b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("Write k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.Write("b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.Write("k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+
+double x = (b2 - b1)/(k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"({x},{y})");
