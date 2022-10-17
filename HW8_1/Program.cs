e6d6ddcc81fa6cc66af52b6ed6d662a661b0ffb0
Elena <senusya@gmail.com>
2022-10-17 18:15:50 +0300

    Add HW 8 task 1

---
+++ HW8_1/Program.cs
@@ -0,0 +1,52 @@
+﻿
+void StreamLines (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+void FillArrayRandom(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1,9);
+    }
+  }
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] array = new int[3, 4];
+Console.WriteLine($"\nUnsorted array: ");
+Console.WriteLine();
+FillArrayRandom(array);
+PrintArray(array);
+Console.WriteLine($"\nSorted array: ");
+Console.WriteLine();
+StreamLines(array);
+PrintArray(array);
