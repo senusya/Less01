f0f720916c7d01119baabec082becfd0af82ae6d
Elena <senusya@gmail.com>
2022-10-17 18:28:22 +0300

    Add HW8 task 3

---
+++ HW8_3/Program.cs
@@ -0,0 +1,55 @@
+﻿int[,] firstMartrix = new int[4, 4];
+FillArrayRandom(firstMartrix);
+Console.WriteLine($"\nFirst Matrix:");
+PrintArray(firstMartrix);
+
+int[,] secomdMartrix = new int[4, 4];
+FillArrayRandom(secomdMartrix);
+Console.WriteLine($"\nSecond Matric:");
+PrintArray(secomdMartrix);
+
+int[,] resultMatrix = new int[4, 4];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nMultiply Matrixes:");
+PrintArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+
+void FillArrayRandom(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1,5);
+    }
+  }
+}
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
