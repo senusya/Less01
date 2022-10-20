d8d15b88745e7463c4589a66e28d3c758e01eee2
Elena <senusya@gmail.com>
2022-10-20 19:02:35 +0300

    Add HW 9 task 1

---
+++ HW9_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write number:");
+int number = int.Parse(Console.ReadLine()!);
+
+string GenerateStringWithNumbers(int number, string str = "") {
+    if (number == 0) {
+        return str;
+    }
+    str = str.Length == 0 ? $"{number}" : $"{str}, {number}";
+    return GenerateStringWithNumbers(number - 1, str) ;
+}
+
+Console.WriteLine($"N = {number} -> \"{GenerateStringWithNumbers(number)}\"");
