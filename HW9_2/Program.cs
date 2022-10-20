516db2574a12ebaaf803e5300cd1089df3d0f0a5
Elena <senusya@gmail.com>
2022-10-20 19:08:21 +0300

    Add HW 9 task 2

---
+++ HW9_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Write M:");
+int m = int.Parse(Console.ReadLine()!);
+int n = 0;
+
+while (n < m) 
+{
+    Console.WriteLine($"Write N > {m}:");
+    n = int.Parse(Console.ReadLine()!);
+}
+
+int GetSum(int from, int to, int sum = 0) {
+    if (to == from) {
+        return sum + to;
+    }
+
+    return GetSum(from + 1, to, sum + from);
+}
+
+Console.WriteLine($"M = {m}; N = {n} -> \"{GetSum(m, n)}\"");
