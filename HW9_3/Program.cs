81e7c4fd0587f41377f78346b463a0cd5a8d25c0
Elena <senusya@gmail.com>
2022-10-20 19:24:30 +0300

    Add HW 9 task 3

---
+++ HW9_3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Write m:");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Write n:");
+int n = int.Parse(Console.ReadLine()!);
+
+int GetAkkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if (m != 0 && n == 0)
+        {
+            return GetAkkerman(m - 1, 1);
+        }
+        else
+        {
+            return GetAkkerman(m - 1, GetAkkerman(m, n - 1));
+        }
+    }
+
+
+}
+
+Console.WriteLine($"m = {m}; n = {n}  -> A(m,n) = {GetAkkerman(m, n)}");
